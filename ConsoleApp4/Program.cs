b85417d052e9d396c340813e795608cb925253d2
217587 <217587@KB37-1-120-C10>
2024-04-03 15:05:58 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class GradeCalculator
+{
+    static void Main()
+    {
+        int dbModuleMax = 22;
+        int softwareDevModuleMax = 38;
+        int softwareMaintenanceModuleMax = 20;
+        int totalScore = 0;
+
+        totalScore += GetModuleScore("Разработка, администрирование и защита баз данных", dbModuleMax);
+        totalScore += GetModuleScore("Разработка модулей программного обеспечения для компьютерных систем", softwareDevModuleMax);
+        totalScore += GetModuleScore("Сопровождение и обслуживание программного обеспечения компьютерных систем", softwareMaintenanceModuleMax);
+
+        Console.WriteLine($"Общая сумма баллов: {totalScore}");
+        Console.WriteLine($"Оценка по 5-бальной шкале: {GetGrade(totalScore)}");
+    }
+
+    static int GetModuleScore(string moduleName, int moduleMaxScore)
+    {
+        Console.WriteLine($"Введите баллы за модуль '{moduleName}' (максимум {moduleMaxScore}): ");
+        int score = int.Parse(Console.ReadLine());
+        return Math.Min(score, moduleMaxScore);
+    }
+
+    static string GetGrade(int totalScore)
+    {
+        if (totalScore >= 56) return "5 (отлично)";
+        if (totalScore >= 32) return "4 (хорошо)";
+        if (totalScore >= 16) return "3 (удовлетворительно)";
+        return "2 (неудовлетворительно)";
+    }
+}
---
+++ ConsoleApp4/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("ConsoleApp4")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ConsoleApp4")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("109803d8-4b2b-4fce-863f-64c0fcdd2e9d")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
